7b6bd1334b2c9ad8ffcf8dac4866c76e1c0f29a9
xqe2011 <gz923553148@gmail.com>
2023-02-12 21:37:21 +0800

    增加了实时数据分析功能

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,20 @@
 import { NestFactory } from '@nestjs/core';
 import { useContainer } from 'class-validator';
 import { AppModule } from './app.module';
+import * as swaggerStats from 'swagger-stats';
 
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
     useContainer(app.select(AppModule), { fallbackOnErrors: true });
+    app.use(swaggerStats.getMiddleware({
+        swaggerSpec: require('./openapi.json'),
+        name: 'Kitty Backend API Statistics',
+        uriPath: '/stats',
+        authentication: true,
+        onAuthenticate: (req, username: string, password: string) => {
+            return((username==='kitty@gtcist.cn') && (password==='kitty') );
+        }
+    }));
     await app.listen(3000);
 }
 bootstrap();
